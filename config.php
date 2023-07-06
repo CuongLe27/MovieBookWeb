ac311ecdaac8410e19d50da65576bd34a87be5e2
CuongLe27 <138785484+CuongLe27@users.noreply.github.com>
2023-07-07 00:20:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "127.0.0.1";
-    $user = "root";
-    $pass = "";
-    $db = "ql_phim";
+    $host = "sql200.infinityfree.com";
+    $user = "if0_34568091";
+    $pass = "y3V1DH6ATm4y3I";
+    $db = "if0_34568091_ql_phim";
     $port = 3306;
      $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 ?>
